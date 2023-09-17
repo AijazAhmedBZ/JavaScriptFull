7371caf9fdb37494a33527194710c40c26673e4d
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2023-09-17 08:20:18 +0500

    updated on 17-Sep-23

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId= 144553
+let accountEmail="example@abc.com"
+var accountPassword="12345"
+accountCity="Karachi"
+
+console.log(accountId, accountEmail, accountPassword, accountCity)
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score) //it converts variable value into number
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = "Aijaz"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Aijaz" => true
