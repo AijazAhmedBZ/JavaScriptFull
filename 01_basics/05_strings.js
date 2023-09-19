8b94862fd3e504913851ef2161290973b41c97a0
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2023-09-19 19:42:04 +0500

    updated on 19-Sep-23

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,15 +1,62 @@
 //string interpolation
 
-const myName = "aijaz"
-const myRepo = 100
+const myName = "aijaz";
+const myRepo = 100;
 
-console.log(`Aoa, My name is ${myName.toUpperCase()} and my repo count is ${myRepo}`)
+// console.log(`Aoa, My name is ${myName.toUpperCase()} and my repo count is ${myRepo}`)
 
-const gameName = new String("ageoe")
+const gameName = new String("ageoe");
 
 // console.log(gameName[0])
 // console.log(gameName.__proto__)
 // console.log(gameName.length)
 // console.log(gameName.toUpperCase())
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf("e"))
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("e"))
+
+const newString = gameName.substring(0, 2);
+// console.log(newString)
+
+const anotherString = gameName.slice(-5, 2);
+// console.log(anotherString)
+
+const name1 = "     Aijaz     ";
+const dy = `${name1}`;
+
+const newStringOne = "     Tahoora     ";
+
+// console.log(newStringOne)
+
+// console.log(dy.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+// console.log(dy)
+// console.log(newStringOne.trim())
+
+const url = "https://aijaz.com/aijaz%20ahmed";
+console.log(url);
+console.log(url.replace("%20", "")); // this is find and replace
+console.log(url.includes("malik")); //returns true or false value after search
+
+const myName1 = "Aijaz-Ahmed-Malik";
+console.log(myName1.split("-"));
---
+++ 01_basics/07-dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date();
+
+// console.log("typeof: ", typeof myDate);
+// console.log("toDateString: ", myDate.toDateString());
+// console.log("toISOString: ", myDate.toISOString());
+// console.log("toJSON: ", myDate.toJSON());
+// console.log("toLocaleDateString: ", myDate.toLocaleDateString());
+// console.log("toLocaleTimeString: ", myDate.toLocaleTimeString());
+// console.log("toString: ", myDate.toString());
+// console.log("toTimeString: ", myDate.toTimeString());
+// console.log("toUTCString: ", myDate.toUTCString());
+
+let myCreatedDate = new Date(2023, 0, 23);
+
+console.log(myCreatedDate);
+console.log(myCreatedDate.toDateString());
