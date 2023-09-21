62f05c4c93321a4ae31dea37afdcca7109f9d825
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2023-09-21 15:29:50 +0500

    updated on 21-Sep-23

--- 01_basics/07-dates.js
+++ 01_basics/07-dates.js
@@ -10,7 +10,25 @@ let myDate = new Date();
 // console.log("toTimeString: ", myDate.toTimeString());
 // console.log("toUTCString: ", myDate.toUTCString());
 
-let myCreatedDate = new Date(2023, 0, 23);
+let myCreatedDate = new Date("01-14-2023");
 
-console.log(myCreatedDate);
-console.log(myCreatedDate.toDateString());
+// console.log("myCreatedDate",myCreatedDate);
+// console.log("toDateString()",myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log("myTimeStamp",myTimeStamp)
+// console.log("myCreatedDate.getTime()",myCreatedDate.getTime())
+// console.log("Math.floor(Date.now())",Math.floor(Date.now()/1000)) //between two time comparisions always do in mili seconds
+// console.log("Math.floor(Date.now())",Math.floor(myTimeStamp/1000)) //between two time comparisions always do in mili seconds
+
+let newdate = new Date()
+
+console.log(newdate)
+console.log(newdate.getMonth()+1)
+console.log(newdate.getDay())
+console.log(newdate.getTime())
+
+// `${newdate.getDay()} and the time is ${newdate.getTime()}`
+
+newdate.toLocaleString
