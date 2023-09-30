7b1d31858c2bd58f6637e263ead56a15dd942676
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2023-09-30 05:03:54 +0500

    updated on 30-Sep-23

--- 07_projects/bmi_calculator/bmi_calculator.js
+++ 07_projects/bmi_calculator/bmi_calculator.js
@@ -6,11 +6,11 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const height = parseInt(document.querySelector("#height").value); //input value will convert into integer
-//   console.log(height)
+  //   console.log(height)
   const weight = parseInt(document.querySelector("#weight").value); //input value will convert into integer
-//   console.log(weight)
+  //   console.log(weight)
   const results = document.querySelector("#results");
-//   console.log(results)
+  //   console.log(results)
 
   if (height === "" || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
@@ -19,6 +19,17 @@ form.addEventListener("submit", function (e) {
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+
+    if (bmi <= 18.6) {
+      underWeight.innerHTML = `<span>You are under weight</span>`;
+    }
+    if (bmi >= 18.6 && bmi <= 24.9) {
+      normalWeight.innerHTML = `<span>Your weight is normal</span>`;
+    }
+    if (bmi > 24.9) {
+      overWeight.innerHTML = `<span>You are over weight</span>`;
+    }
     results.innerHTML = `<span>${bmi}</span>`;
   }
 });
